bf094b98660f0320951804a75dca13a424a9f946
RaminSafarli <ramin.seferli.kb@gmail.com>
2023-03-24 02:06:28 +0400

    'popular-author-and-related-books'

---
+++ BookStore/BookStore.Domain/Business/AuthorModule/PopularAuthorsQuery.cs
@@ -0,0 +1,50 @@
+﻿using BookStore.Domain.Models.DataContexts;
+using BookStore.Domain.Models.Entities;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BookStore.Domain.Business.BookModule
+{
+    public class PopularAuthorsQuery : IRequest<List<Author>>
+    {
+
+        public int Size { get; set; }
+
+
+        public class PopularAuthorsQueryHandler : IRequestHandler<PopularAuthorsQuery, List<Author>>
+        {
+            private readonly BookStoreDbContext db;
+
+            public PopularAuthorsQueryHandler(BookStoreDbContext db)
+            {
+                this.db = db;
+            }
+
+            public async Task<List<Author>> Handle(PopularAuthorsQuery request, CancellationToken cancellationToken)
+            {
+                var authorOrders = await db.Orders
+    .SelectMany(o => o.OrderBooks.Select(ob => new { ob.Book.Author, o }))
+    .ToListAsync(cancellationToken);
+
+                var authorGroupedOrders = authorOrders
+                    .GroupBy(x => x.Author)
+                    .Select(g => new
+                    {
+                        Author = g.Key,
+                        NumOrders = g.Select(x => x.o.Id).Distinct().Count()
+                    })
+                    .OrderByDescending(x => x.NumOrders)
+                    .ToList();
+
+                var result = authorGroupedOrders.Select(x => x.Author).ToList();
+                return result;
+
+
+            }
+        }
+    }
+}
--- BookStore/BookStore.WebUI/AppCode/ViewComponents/RelatedBooksViewComponent.cs
+++ BookStore/BookStore.WebUI/AppCode/ViewComponents/RelatedBooksViewComponent.cs
@@ -1,23 +1,22 @@
-﻿using BookStore.Domain.Business.AuthorModule;
-using BookStore.Domain.Business.BookModule;
+﻿using BookStore.Domain.Business.BookModule;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
 
 namespace BookStore.WebUI.AppCode.ViewComponents
 {
-    public class PopularAuthorsViewComponent : ViewComponent
+    public class RelatedBooksViewComponent : ViewComponent
     {
         private readonly IMediator mediator;
 
-        public PopularAuthorsViewComponent(IMediator mediator)
+        public RelatedBooksViewComponent(IMediator mediator)
         {
             this.mediator = mediator;
         }
 
-        public async Task<IViewComponentResult> InvokeAsync()
+        public async Task<IViewComponentResult> InvokeAsync(string categoryName)
         {
-            var query = new PopularAuthorsQuery() { Size = 7 };
+            var query = new RelatedBooksQuery() {Category = categoryName};
             var books = await mediator.Send(query);
 
             return View(books);
